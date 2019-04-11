4282cc2954464fc54442f269d70cdc4416d6b9bc
javierc22 <javier.ec.22@gmail.com>
2019-04-10 22:01:40 -0400

    Testing modelo Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    # Valida que el nombre esté presente
+    validates :name, presence: true
+
+    # valida que el nombre sea único y que no sea sensible a mayúsculas
+    validates :name, uniqueness: { case_sensitive: false }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # La categoría debe tener nombre
+    it { should validate_presence_of :name}
+
+    # el nombre debe ser único y sensible a mayusculas
+    it { should validate_uniqueness_of(:name).case_insensitive }
 end
