5f3ed1650041d1cbc0e30a1adb0812e2e7ef595b
javierc22 <javier.ec.22@gmail.com>
2019-05-05 12:18:07 -0400

    Test de autenticación de usuario

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,26 @@
+# define a command class
+class AuthenticateUser
+    # put SimpleCommand before the class' ancestors chain
+    prepend SimpleCommand
+
+    # optional, initialize the command with some arguments
+    def initialize(email, password)
+        @email = email
+        @password = password
+    end
+
+    def call
+        JsonWebToken::encode(user_id: user.id) if user
+    end
+
+    private
+
+    attr_accessor :email, :password
+  
+    def user
+        user = User.find_by_email(email)
+        return user if user && user.authenticate(password)
+    
+        errors.add :user_authentication, 'Invalid credentials'
+    end
+end
---
+++ spec/commands/authenticate_user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticateUser do
+
+    let(:user) { create :user, password: 'password'}
+
+    subject(:valid_auth) { described_class.new(user.email, 'password') } # autenticación válida
+    subject(:invalid_auth) { described_class.new('foo', 'bar') } # autenticación invalida
+
+    
+    describe ".call" do
+        # Contexto con credenciales válidas
+        context "when valid credentials" do
+            it "returns an auth token" do
+                require "pry"; binding.pry
+                token = valid_auth.call
+                expect(token).not_to be_nil # Espera a que el "token" no sea nulo
+            end
+        end
+
+        # Contexto con credenciales inválidas
+        context "when invalid credentials" do
+            it "raises an authentication error" do
+                result = invalid_auth.call
+                expect(result.errors[:user_authentication]).to eq ["Invalid credentials"] # Espera que el resultado sea un error
+            end
+        end
+    end
+        
+end
